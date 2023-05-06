60f11caeb39f953eed2215ea499f59d1c1924eeb
Miroslav <gavrilov.miroslav@gmail.com>
2023-05-06 22:21:44 +0200

    Gitignore merged

---
+++ src/ast.rs
@@ -0,0 +1,66 @@
+
+#[derive(Debug)]
+pub enum VarType {
+    IntType,
+    BoolType,
+    FloatType,
+    StringType,
+}
+
+#[derive(Debug)]
+pub enum TopLevelExpr {
+    Actor { actor_name: String, content: Vec<ActorExpr> },
+    Event { event_name: String, params: Vec<VarType> },
+    Func { func_name: String, params: Vec<(String, VarType)>, ret_type: Option<VarType>, body: Vec<ControlFlowExpr> },
+}
+
+#[derive(Debug)]
+pub enum ActorExpr {
+    VarDecl { var_name: String, var_type: VarType, initial: Option<ValueExpr> },
+    StateMachine(Vec<StateMachineExpr>),
+    TransitionDecl { event: ValueExpr, conditions: Vec<ValueExpr>, target: String, body: Vec<ControlFlowExpr> },
+    EntryDecl(Vec<ControlFlowExpr>),
+    ExitDecl(Vec<ControlFlowExpr>),
+}
+
+#[derive(Debug)]
+pub enum StateMachineExpr {
+    VarDecl { var_name: String, var_type: VarType, initial: Option<ValueExpr> },
+    InitialStateDecl(String),
+    StateDecl { state_name: String, content: Vec<StateMachineExpr> },
+    TransitionDecl { event: ValueExpr, conditions: Vec<ValueExpr>, target: String, body: Vec<ControlFlowExpr> },
+    EntryDecl(Vec<ControlFlowExpr>),
+    ExitDecl(Vec<ControlFlowExpr>),
+}
+
+#[derive(Debug)]
+pub enum ControlFlowExpr {
+    VarDecl { var_name: String, var_type: VarType, initial: Option<ValueExpr> },
+    SendStatement { target_state: String, event: ValueExpr },
+    AssignStatement { var_name: String, val_expr: ValueExpr },
+    FuncCallStatement(ValueExpr),
+}
+
+#[derive(Debug)]
+pub enum ValueExpr {
+    Bool(bool),
+    Int(i64),
+    Float(f64),
+    Str(String),
+    Ident(String),
+    AddExpr { l: Box<ValueExpr>, r: Box<ValueExpr> },
+    SubExpr { l: Box<ValueExpr>, r: Box<ValueExpr> },
+    MulExpr { l: Box<ValueExpr>, r: Box<ValueExpr> },
+    DivExpr { l: Box<ValueExpr>, r: Box<ValueExpr> },
+    NotExpr { v: Box<ValueExpr> },
+    OrExpr  { l: Box<ValueExpr>, r: Box<ValueExpr> },
+    AndExpr { l: Box<ValueExpr>, r: Box<ValueExpr> },
+    XorExpr { l: Box<ValueExpr>, r: Box<ValueExpr> },
+    EqExpr  { l: Box<ValueExpr>, r: Box<ValueExpr> },
+    NeqExpr { l: Box<ValueExpr>, r: Box<ValueExpr> },
+    LeqExpr { l: Box<ValueExpr>, r: Box<ValueExpr> },
+    GeqExpr { l: Box<ValueExpr>, r: Box<ValueExpr> },
+    LtExpr  { l: Box<ValueExpr>, r: Box<ValueExpr> },
+    GtExpr  { l: Box<ValueExpr>, r: Box<ValueExpr> },
+    FuncCallExpr { func_name: String, func_args: Vec<ValueExpr> },
+}
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+mod ast;
+
+use lalrpop_util::lalrpop_mod;
+lalrpop_mod!(pub proteus);
+
+fn main() {
+    println!("{:?}", proteus::ExprParser::new().parse("!(22 + \"qwe\" / x * foo(a, 2))"));
+    println!("{:?}", proteus::ControlFlowParser::new().parse("int x = 5;"));
+    println!("{:?}", proteus::ControlFlowParser::new().parse("x ! Foo(5);"));
+    println!("{:?}", proteus::ControlFlowParser::new().parse("x = 7;"));
+    println!("{:?}", proteus::ControlFlowParser::new().parse("Foo(1, x, \"qwe\");"));
+    println!("{:?}", proteus::StateMachineParser::new().parse("state Foo { state A { on Foo(e) goto C {}; }; initial A; }"));
+    println!("{:?}", proteus::TopLevelParser::new().parse("actor A {}"));
+    println!("{:?}", proteus::TopLevelParser::new().parse("event PowerOn();"));
+    println!("{:?}", proteus::TopLevelParser::new().parse("event Capture(int);"));
+    println!("{:?}", proteus::TopLevelParser::new().parse("func add(int x, int y) -> int {}"));
+}
